68a44294d16aafa0507c3a0495bfa405ba24df4e
Martin Møller Skarbiniks Pedersen <mp@superusers.dk>
2022-08-16 13:02:51 +0200

    Update runit.sh

--- 5-limits/cpu/runit.sh
+++ 5-limits/cpu/runit.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+docker container run --cpu-shares 512
+
+bruger@superusers:~/SU0160/5-limits/cpu$ cat runit.sh 
+#!/bin/bash
+
+if ! docker image ls | grep -q su0160/cpu-counter  
+then
+  docker build -t su0160/cpu-counter .  
+fi
+
 docker container run -d --cpu-shares  500   --name counter2    su0160/cpu-counter
 docker container run -d --cpu-shares  250   --name counter1    su0160/cpu-counter
 docker container run -d --cpus 0.75  --name counter3    su0160/cpu-counter
@@ -8,10 +18,11 @@ docker container run -d --cpuset-cpus 0,1 --name counter4b    su0160/cpu-counter
 docker container run -d --cpuset-cpus 0-2 --name counter4c    su0160/cpu-counter
 docker container run -d --cpuset-cpus 0 --name counter4d    su0160/cpu-counter
 
+echo "Running for 60 seconds"
 sleep 60
-docker logs counter3 | tail -1
-docker logs counter2   | tail -1 
-docker logs counter1   | tail -1
 
-docker container kill counter1 counter2 counter3 counter4a counter4b counter4c counter4d
-docker container rm counter1 counter2 counter3 counter4a counter4b counter4c counter4d
+docker logs counter3 | tail -1
+docker logs counter2 | tail -1 
+docker logs counter1 | tail -1
+
+docker container rm -f counter1 counter2 counter3 counter4a counter4b counter4c counter4d >/de
