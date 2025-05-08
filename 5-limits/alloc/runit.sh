5273bd0f1d77baa37acb5eaa4dbf82895baa2f97
Søren Reinke <soren@reinke.dk>
2025-05-08 15:00:59 +0200

    update

--- 5-limits/alloc/runit.sh
+++ 5-limits/alloc/runit.sh
@@ -7,12 +7,12 @@ fi
 
 clear
 echo "Running with 128M"
-sleep 5
+sleep 2
 docker container run --memory=128M --rm su0160/calloc 
-sleep 10
+sleep 5
 
 clear
 echo "Running with 1G"
-sleep 5
+sleep 2
 docker container run --memory=1G --rm su0160/calloc 
-sleep 10
+sleep 5
